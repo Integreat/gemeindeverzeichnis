3babb7f08a7d3e69ba044b048e4dce629046e7e0
Sven Seeberg <mail@sven-seeberg.de>
2019-04-12 21:46:01 +0200

    Add end points and database

--- ajax.php
+++ ajax.php
@@ -1,5 +1,19 @@
 <?php
 $cfg = parse_ini_file("config.ini");
-include("includes/map.php")
+require_once("includes/database.php");
+
+if("/api/quicksearch" == $_SERVER['REQUEST_URI']) {
+    require_once("includes/quicksearch.php");
+}
+elseif("/api/details" == $_SERVER['REQUEST_URI']) {
+    require_once("includes/details.php");
+}
+elseif("/api/results" == $_SERVER['REQUEST_URI']) {
+    require_once("includes/results.php");
+}
+else
+{
+    json_encode(array("error" => "end point not found"));
+}
 
 ?>
