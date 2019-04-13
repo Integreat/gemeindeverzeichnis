5c972a91b2b084344161a7a612fd38742e823dbc
Sven Seeberg <mail@sven-seeberg.de>
2019-04-13 16:02:58 +0200

    Search result end point

--- includes/quicksearch.php
+++ includes/quicksearch.php
@@ -1,14 +1,15 @@
 <?php
 $content = file_get_contents('php://input');
 $query = json_decode($content)[0];
+$query = "%{$query}%";
 
 if(is_numeric($query)) {
-    $stmt = $conn->prepare("SELECT zip FROM zip_codes WHERE zip LIKE ? LIMIT 8");
-    $stmt->bind_param('s', "%{$query}%");
+    $stmt = $conn->prepare("SELECT zip FROM zip_codes WHERE zip LIKE ? AND valid=1 LIMIT 8");
+    $stmt->bind_param('s', $query});
     $field = "zip";
 }else{
-    $stmt = $conn->prepare("SELECT name FROM municipalities WHERE name LIKE ? LIMIT 8");
-    $stmt->bind_param('s', "%{$query}%");
+    $stmt = $conn->prepare("SELECT name FROM municipalities WHERE name LIKE ? AND valid=1 LIMIT 8");
+    $stmt->bind_param('s', $query);
     $field = "name";
 }
 $stmt->execute();
---
+++ includes/results.php
@@ -0,0 +1,29 @@
+<?php
+$content = file_get_contents('php://input');
+$query = json_decode($content)[0];
+$query = "%{$query}%";
+
+
+if(is_numeric($query)) {
+    $sql = "SELECT m.key, m.name, z.zip, m.state FROM municipalities m LEFT JOIN zip_codes z ON m.key = z.municipality_key WHERE z.zip LIKE ? AND m.valid=1";
+}else{
+    $sql = "SELECT key, name, zip, state FROM municipalities WHERE name LIKE ? AND valid=1";
+}
+
+$stmt = $conn->prepare($sql);
+$stmt->bind_param('s', $query);
+$stmt->execute();
+$res = $stmt->get_result();
+
+$result = array();
+$n = 0;
+while($row = $res->fetch_assoc()) {
+    $result[$n]['key'] = $row['key'];
+    $result[$n]['name'] = $row['name'];
+    $result[$n]['zip'] = $row['zip'];
+    $result[$n]['state'] = $row['state'];
+}
+
+header("Content-Type: application/json");
+echo json_encode($result);
+?>
