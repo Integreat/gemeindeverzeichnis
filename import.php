9f8283bcbde8c48a6191442027d27091929cad96
Sven Seeberg <mail@sven-seeberg.de>
2019-04-13 23:10:24 +0200

    Import works

--- import.php
+++ import.php
@@ -1,101 +1,102 @@
 <?php
 $cfg = parse_ini_file("config.ini");
 require_once("includes/database.php");
+if(in_array('--drop', $argv)) {
+    $stmt = $conn->prepare("DELETE FROM `municipalities`");
+    $stmt->execute();
+    $stmt->close();
 
-$stmt = $conn->prepare("DELETE FROM `municipalities`");
-$stmt->execute();
-$stmt->close();
+    $stmt = $conn->prepare("DELETE FROM `zip_codes`");
+    $stmt->execute();
+    $stmt->close();
 
-$stmt = $conn->prepare("DELETE FROM `zip_codes`");
-$stmt->execute();
-$stmt->close();
+    $content = explode("\n", file_get_contents("data-base.csv"));
 
-$content = explode("\n", file_get_contents("data-base.csv"));
+    /**
+     *  1. Satzkennzeichen
+     *  2. Textkennzeichen
+     *  3. Regionalschlüssel - Land
+     *  4. Regionalschlüssel - Regierungsbezirk
+     *  5. Regionalschlüssel - Kreis
+     *  6. Regionalschlüssel - Verwaltungsbezirk
+     *  7. Regionalschlüssel - Gemeinde
+     *  8. Gemeindename
+     *  9. Fläche
+     * 10. Bevölkerung - insgemsamt
+     * 11. Bevölkerung - männlich
+     * 12. Bevölkerung - weiblich
+     * 13. Bevölkerung - pro km²
+     * 14. PLZ
+     * 15. Längengrad
+     * 16. Breitengrad
+     * ...
+     */
 
-/**
- *  1. Satzkennzeichen
- *  2. Textkennzeichen
- *  3. Regionalschlüssel - Land
- *  4. Regionalschlüssel - Regierungsbezirk
- *  5. Regionalschlüssel - Kreis
- *  6. Regionalschlüssel - Verwaltungsbezirk
- *  7. Regionalschlüssel - Gemeinde
- *  8. Gemeindename
- *  9. Fläche
- * 10. Bevölkerung - insgemsamt
- * 11. Bevölkerung - männlich
- * 12. Bevölkerung - weiblich
- * 13. Bevölkerung - pro km²
- * 14. PLZ
- * 15. Längengrad
- * 16. Breitengrad
- * ...
- */
+    $types = array();
+    $types[60] = "Markt";
+    $types[61] = "Kreisfreie Stadt";
+    $types[62] = "Stadtkreis";
+    $types[63] = "Stadt";
+    $types[64] = "Kreisangehörige Gemeinde";
+    $types[65] = "gemeindefreies Gebiet, bewohnt";
+    $types[66] = "gemeindefreies Gebiet, unbewohnt";
+    $types[67] = "große Kreisstadt";
 
-$types = array();
-$types[60] = "Markt";
-$types[61] = "Kreisfreie Stadt";
-$types[62] = "Stadtkreis";
-$types[63] = "Stadt";
-$types[64] = "Kreisangehörige Gemeinde";
-$types[65] = "gemeindefreies Gebiet, bewohnt";
-$types[66] = "gemeindefreies Gebiet, unbewohnt";
-$types[67] = "große Kreisstadt";
+    $state = '';
+    $county = '';
+    $district = '';
 
-$state = '';
-$county = '';
-$district = '';
-
-foreach($content as $row) {
-    $columns = explode(";", $row);
-    if($columns[0] == '10') { //Bundesland
-        $state = $columns[7];
-        $county = '';
-        $district = '';
-    }
-    elseif($columns[0] == '20') { //Regierungsbezirk
-        $district = $columns[7];
-        $county = '';
-    }
-    elseif($columns[0] == '40') { //Landkreise
-        $county = $columns[7];
-    }
-    elseif($columns[0] == '50') { //Gemeindeverband
-        // Nothing to do so far
-    }
-    elseif($columns[0] == '60') { //Gemeinden
-        $rs = $columns[2] . $columns[3] . $columns[4] . $columns[6];
-        $name = $columns[7];
-        $zip = $columns[13];
-        $type = $types[(int)$columns[1]];
-        $pop = str_replace(" ", "", $columns[9]);
-        $pop_male = str_replace(" ", "", $columns[10]);
-        $pop_female = str_replace(" ", "", $columns[11]);
-        $longitude = str_replace(",", ".", $columns[14]);
-        $latitude = str_replace(",", ".", $columns[15]);
-        $stmt = $conn->prepare("INSERT INTO `municipalities`
-        (`key`, `name`, `county`, `state`, `district`, `type`, `population`, `population_male`, `population_female`, `longitude` , `latitude`,   `area`     , `valid`) VALUES
-        ( ?   ,  ?    ,  ?      ,  ?     ,  ?        ,  ?    ,  ?          ,  ?               ,  ?                 ,  ?          ,  ?        ,    ?         , '0'    )");
-        $stmt->bind_param("ssssssssssss",
-        $rs   ,  $name, $county , $state , $district , $type , $pop        , $pop_male        , $pop_female        , $longitude  , $latitude, $columns[8]);
-        if($stmt->execute()) {
-            $stmt->close();
-            $stmt = $conn->prepare("INSERT INTO zip_codes (`municipality_key`, `zip`) VALUES (?, ?)");
-            $stmt->bind_param("ss", $rs, $zip);
-            if($stmt->execute()){
-                echo "Stored $rs.\n";
+    foreach($content as $row) {
+        $columns = explode(";", $row);
+        if($columns[0] == '10') { //Bundesland
+            $state = $columns[7];
+            $county = '';
+            $district = '';
+        }
+        elseif($columns[0] == '20') { //Regierungsbezirk
+            $district = $columns[7];
+            $county = '';
+        }
+        elseif($columns[0] == '40') { //Landkreise
+            $county = $columns[7];
+        }
+        elseif($columns[0] == '50') { //Gemeindeverband
+            // Nothing to do so far
+        }
+        elseif($columns[0] == '60') { //Gemeinden
+            $rs = $columns[2] . $columns[3] . $columns[4] . $columns[6];
+            $name = $columns[7];
+            $zip = $columns[13];
+            $type = $types[(int)$columns[1]];
+            $pop = str_replace(" ", "", $columns[9]);
+            $pop_male = str_replace(" ", "", $columns[10]);
+            $pop_female = str_replace(" ", "", $columns[11]);
+            $longitude = str_replace(",", ".", $columns[14]);
+            $latitude = str_replace(",", ".", $columns[15]);
+            $stmt = $conn->prepare("INSERT INTO `municipalities`
+            (`key`, `name`, `county`, `state`, `district`, `type`, `population`, `population_male`, `population_female`, `longitude` , `latitude`,   `area`     , `valid`) VALUES
+            ( ?   ,  ?    ,  ?      ,  ?     ,  ?        ,  ?    ,  ?          ,  ?               ,  ?                 ,  ?          ,  ?        ,    ?         , '0'    )");
+            $stmt->bind_param("ssssssssssss",
+            $rs   ,  $name, $county , $state , $district , $type , $pop        , $pop_male        , $pop_female        , $longitude  , $latitude, $columns[8]);
+            if($stmt->execute()) {
+                $stmt->close();
+                $stmt = $conn->prepare("INSERT INTO zip_codes (`municipality_key`, `zip`) VALUES (?, ?)");
+                $stmt->bind_param("ss", $rs, $zip);
+                if($stmt->execute()){
+                    echo "Stored $rs.\n";
+                } else {
+                    echo "Failed to store ZIP code for $rs.\n";
+                }
+                $stmt->close();
             } else {
-                echo "Failed to store ZIP code for $rs.\n";
+                $stmt->close();
+                echo "Failed to store $rs.\n";
             }
-            $stmt->close();
-        } else {
-            $stmt->close();
-            echo "Failed to store $rs.\n";
         }
     }
 }
 
-if($argv[1]=="--file") {
+if(in_array('--file', $argv)) {
     echo "Updating details from file.\n";
     /**
      *  1. Stand
@@ -132,7 +133,7 @@ if($argv[1]=="--file") {
      * -H 'Accept: application/json' -H 'Application/x-www-form-urlencoded; charset=UTF-8' \
      * --data 'mi_search=01051064&form_id=municipality_index_search'
      */
-    $stmt = $conn->prepare("SELECT `key` FROM `municipalities` LIMIT 1");
+    $stmt = $conn->prepare("SELECT `key` FROM `municipalities`");
     $stmt->execute();
     $res = $stmt->get_result();
     while($row = $res->fetch_assoc()) {
@@ -156,7 +157,7 @@ if($argv[1]=="--file") {
         xml_parse_into_struct($p, $xml, $vals, $index);
         $data = array();
         foreach($vals as $item) {
-            if(!in_array('value', $item)){
+            if(!array_key_exists('value', $item)){
                 continue;
             }
             $value = trim($item['value']);
@@ -203,7 +204,7 @@ if($argv[1]=="--file") {
         $data['population_male'] = str_replace(" ", "", $data['population_male']);
         $data['population_female'] = str_replace(" ", "", $data['population_female']);
         $stmt = $conn->prepare("UPDATE `municipalities` SET `county` = ?, `state` = ?, `district` = ?, `type` = ?, `population` = ?, `population_male` = ?, `population_female` = ?, `area` = ?, `address_recipient` = ?, `address_street` = ?, `address_zip` = ?, `address_city` = ?, `valid`=1 WHERE `key` = ?");
-        $stmt->bind_param("sssssssssssss", $data['county'], $data['state'], $data['district'], $data['type'], $data['population'], $data['population_male'], $data['population_female'], $data['area'], $data['address_recipient'], $data['address_street'], $data['address_zip'], $data['address_city'], $row['key']);
+        $stmt->bind_param("sssssssssssss", $data['county'], $data['state'], $data['district'], $data['type'], $data['population'], $data['population_male'], $data['population_female'], $data['area'], $data['address_recipient'], $data['address_street'], $address_zip, $address_city, $row['key']);
         if($stmt->execute()) {
             echo "Updated ".$row['key']."\n";
         }
