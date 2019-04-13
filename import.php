fd89d8a7e2b139f26f50498be317ba897e58325d
Sven Seeberg <mail@sven-seeberg.de>
2019-04-13 13:54:55 +0200

    Add import of polling stations

--- import.php
+++ import.php
@@ -61,4 +61,30 @@ foreach($content as $row) {
     }
 }
 
+/**
+ *  1. Stand
+ *  2. Bundesland
+ *  3. Regierungs-Bezirk
+ *  4. Kreisname
+ *  5. Amtl.Gemeindeschlüssel
+ *  6. PLZ Gemeindenamen
+ *  7. Gemeindetyp
+ *  8. Anschrift der Gemeinde
+ *  9. Straße
+ * 10. PLZ Ort
+ * 11. Fläche km2
+ * 12. Einwohner gesamt
+ * 13. Einwohner männlich
+ * 14. Einwohner weiblich
+ * 15. Einwohner je km2
+ */
+
+$content = explode("\n", file_get_contents("data-station.csv"));
+foreach($content as $row) {
+    $columns = explode(";", $row);
+    $stmt = $conn->prepare("UPDATE `municipalities` SET `ps_name`=?, `ps_street`=?, `ps_zip`=?, `ps_city`=?, `timestramp`, `valid`=1) WHERE `key` = ?");
+$stmt->bind_param("sssss", $columns[7], $columns[8], $columns[9], $columns[4]);
+if($stmt->execute()) {
+    echo "Updated $columns[4].\n";
+}
 ?>
