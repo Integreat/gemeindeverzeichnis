fa657775fb68a285b8c2c9b2e08ddc48258ba565
Sven Seeberg <mail@sven-seeberg.de>
2019-04-14 16:38:21 +0200

    Decode URL, fixes #11

--- ajax.php
+++ ajax.php
@@ -7,7 +7,7 @@ $route = trim($_SERVER['REQUEST_URI'], "/");
 $route = explode("/", $route);
 
 if(isset($route[2])) {
-    $query = $route[2];
+    $query = urldecode($route[2]);
 } else {
     $content = file_get_contents('php://input');
     $query = json_decode($content)[0];
