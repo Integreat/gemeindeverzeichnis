20a44770cee3b619ce81b953c9b1091abb0a0dff
Sven Seeberg <mail@sven-seeberg.de>
2019-04-14 14:23:37 +0200

    Use bootstrap table

--- scripts.js
+++ scripts.js
@@ -40,19 +40,19 @@ function search( term ) {
 function details( key ) {
     $.get("/api/details/"+key, function(data, status){
         var searchhtml;
-        searchhtml = "<div class='container'>";
+        searchhtml = '<table class="table table-striped">';
         for (item of data) {
-            searchhtml = searchhtml + "<div class='row'><div class='col'>"+item['name']+"</div></div>" +
-            "<div class='row'><div class='col'>Regierungsbezirk</div><div class='col'>"+item['district']+"</div></div>" +
-            "<div class='row'><div class='col'>Bundesland</div><div class='col'>"+item['state']+"</div></div>" +
-            "<div class='row'><div class='col'>Landkreis</div><div class='col'>"+item['county']+"</div></div>" +
-            "<div class='row'><div class='col'>Gemeindetyp</div><div class='col'>"+item['type']+"</div></div>" +
-            "<div class='row'><div class='col'>Anschrift</div><div class='col'>"+item['address']['recipient']+", "+item['address']['street']+", "+item['address']['zip']+" "+item['address']['city']+"</div></div>" +
-            "<div class='row'><div class='col'>L&auml;ngengrad</div><div class='col'>"+item['longitude']+"</div></div>" +
-            "<div class='row'><div class='col'>Breitengrad</div><div class='col'>"+item['latitude']+"</div></div>" +
-            "<div class='row'><div class='col'>Bevölkerung</div><div class='col'>"+item['population']+"</div></div>";
+            searchhtml = searchhtml + "<thead><tr><th scope='col' colspan='2'>"+item['name']+"</th></tr></thead>" +
+            "<tr><th scope='row'>Regierungsbezirk</th><td>"+item['district']+"</td></tr>" +
+            "<tr><th scope='row'>Bundesland</th><td>"+item['state']+"</td></tr>" +
+            "<tr><th scope='row'>Landkreis</th><td>"+item['county']+"</td></tr>" +
+            "<tr><th scope='row'>Gemeindetyp</th><td>"+item['type']+"</td></tr>" +
+            "<tr><th scope='row'>Anschrift</th><td>"+item['address']['recipient']+", "+item['address']['street']+", "+item['address']['zip']+" "+item['address']['city']+"</td></tr>" +
+            "<tr><th scope='row'>L&auml;ngengrad</th><td>"+item['longitude']+"</td></tr>" +
+            "<tr><th scope='row'>Breitengrad</th><td>"+item['latitude']+"</td></tr>" +
+            "<tr><th scope='row'>Bevölkerung</th><td>"+item['population']+"</td></tr>";
         };
-        searchhtml = searchhtml + "</div>";
+        searchhtml = searchhtml + "</table>";
         $("#result").html(searchhtml);
     });
 }
