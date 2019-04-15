f75e2beea369a1d95e31e80c366208cc1389d5e2
Sven Seeberg <mail@sven-seeberg.de>
2019-04-15 18:27:07 +0200

    Import based on zips, import all param

--- import.php
+++ import.php
@@ -2,21 +2,21 @@
 $cfg = parse_ini_file("config.ini");
 require_once("includes/database.php");
 
-if(in_array('--base', $argv)) {
+if(in_array('--base', $argv) or in_array('--all', $argv)) {
     require_once("includes/import-base.php");
 }
 
-if(in_array('--file', $argv)) {
+if(in_array('--file', $argv) or in_array('--all', $argv)) {
     //import file from https://github.com/digineo/gemeindeverzeichnis
     require_once("includes/import-details-file.php");
 }
 
-if(in_array('--webupdate', $argv)) {
+if(in_array('--webupdate', $argv) or in_array('--all', $argv)) {
     //Crawl data from destatis
     require_once("includes/import-details-www.php");
 }
 
-if(in_array('--homepage', $argv)) {
+if(in_array('--homepage', $argv) or in_array('--all', $argv)) {
     //Crawl data from destatis
     require_once("includes/import-homepages.php");
 }
--- includes/import-homepages.php
+++ includes/import-homepages.php
@@ -1,41 +1,40 @@
 <?php
 
-$content = file_get_contents("data-homepages.csv");
-$content = explode("\n", $content);
-
-foreach($content as $row) {
-    $columns = explode(',', $row);
-    if($columns[1] == "") {
-        continue;
-    }
-    echo "Searching for ".$columns[0]."\n";
-    $key = "";
-    $stmt = $conn->prepare("SELECT `key` FROM `municipalities` WHERE `name`=? OR `name`=?");
-    $stadt = $columns[0].", Stadt";
-    $stmt->bind_param('ss',$columns[0], $stadt);
-    $stmt->execute();
-    $result = $stmt->get_result();
-    if($result->num_rows == 1) {
-        $key = $result->fetch_assoc()['key'];
-    }
-    $stmt->close();
-    if(!$key) {
-        $stmt = $conn->prepare("SELECT `key` FROM `municipalities` WHERE `name`LIKE ?");
-        $search = "%".$columns[0]."%";
-        $stmt->bind_param('s', $search);
+if (($handle = fopen("data-homepages.csv", "r")) !== FALSE) {
+    while (($columns = fgetcsv($handle, 1000, ",", '"')) !== FALSE) {
+        $row++;
+        if($columns[1] == "") {
+            continue;
+        }
+        echo "Searching for ".$columns[0]."\n";
+        $key = "";
+        $stmt = $conn->prepare("SELECT `key` FROM `municipalities` WHERE `address_zip`=?");
+        $stmt->bind_param('s',substr($columns[2],0,5));
         $stmt->execute();
         $result = $stmt->get_result();
         if($result->num_rows == 1) {
             $key = $result->fetch_assoc()['key'];
         }
         $stmt->close();
-    }
-    if($key) {
-        $stmt = $conn->prepare("UPDATE `municipalities` SET `website`=? WHERE `key` = ?");
-        $stmt->bind_param("ss", $columns[1], $key);
-        if($stmt->execute()) {
-            echo "Updated $key $columns[0] $columns[1]\n";
+        if(!$key) {
+            $stmt = $conn->prepare("SELECT `key` FROM `municipalities` WHERE `name`LIKE ?");
+            $search = "%".$columns[0]."%";
+            $stmt->bind_param('s', $search);
+            $stmt->execute();
+            $result = $stmt->get_result();
+            if($result->num_rows == 1) {
+                $key = $result->fetch_assoc()['key'];
+            }
+            $stmt->close();
         }
-        $stmt->close();
+        if($key) {
+            $stmt = $conn->prepare("UPDATE `municipalities` SET `website`=? WHERE `key` = ?");
+            $stmt->bind_param("ss", $columns[1], $key);
+            if($stmt->execute()) {
+                echo "Updated $key $columns[0] $columns[1]\n";
+            }
+            $stmt->close();
+        }
     }
 }
+fclose($handle);
