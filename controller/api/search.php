e0c7681e374ee3f0d255088d242ee1aab523ecc7
Sven Seeberg <mail@sven-seeberg.de>
2019-04-22 15:39:48 +0200

    Fix zip code search

--- controller/api/search.php
+++ controller/api/search.php
@@ -5,11 +5,11 @@ use Integreat\Gemeindeverzeichnis\DatabaseConnection;
 
 $conn = Container::getInstance()->get(DatabaseConnection::class);
 
-$query = "%{$query}%";
-
 if(is_numeric($query)) {
+    $query = "%{$query}%";
     $sql = "SELECT `m`.`key`, `m`.`name`, `z`.`zip`, `m`.`county`, `m`.`state` FROM `municipalities` `m` LEFT JOIN zip_codes z ON `m`.`key` = `z`.`municipality_key` WHERE `z`.`zip` LIKE ?";
 }else{
+    $query = "%{$query}%";
     $sql = "SELECT `key`, `name`, `address_zip`, `state`, `county` FROM `municipalities` WHERE `name` LIKE ?";
 }
 
