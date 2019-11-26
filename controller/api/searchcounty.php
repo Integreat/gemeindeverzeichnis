6ad926139de6ae11a9767c4a80116333506ec17b
Sven Seeberg <mail@sven-seeberg.de>
2019-11-26 18:28:46 +0100

    Fix county children sql query

--- controller/api/searchcounty.php
+++ controller/api/searchcounty.php
@@ -21,7 +21,7 @@ while($row = $res->fetch_assoc()) {
     $result[$n]['name'] = $row['name'];
     $result[$n]['state'] = $row['state'];
 
-    $sql = "SELECT `key`, `name`, `address_zip`, `county`, `state` FROM `municipalities` WHERE `parent_key`=? AND type_code=60";
+    $sql = "SELECT `key`, `name`, `address_zip`, `county`, `state` FROM `municipalities` WHERE `parent_key`=? AND type_category=60";
 
     $stmt = $conn->prepare($sql);
     $filter_key = $row['key'];
